8f71a70e423aa5b8cf5574dc3bbb04e9dec70391
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-06-19 11:05:46 +0900

    8-1. JSON - ゼロ値と区別したomitempty

--- chapter.8/8-1/main.go
+++ chapter.8/8-1/main.go
@@ -5,15 +5,22 @@ import (
 	"fmt"
 )
 
-type FormInput struct {
-	Name        string `json:"name"`
-	CompanyName string `json:"company_name,omitempty"`
+type Bottle struct {
+	Name  string `json:"name"`
+	Price int    `json:"price,omitempty"`
+	KCal  *int   `json:"kcal,omitempty"`
 }
 
 func main() {
-	in := FormInput{
-		Name: "山田太郎",
+	b := Bottle{
+		Name:  "ミネラルウォーター",
+		Price: 0,
+		KCal:  Int(0),
 	}
-	b, _ := json.Marshal(in)
-	fmt.Println(string(b))
+	out, _ := json.Marshal(b)
+	fmt.Println(string(out))
+}
+
+func Int(v int) *int {
+	return &v
 }
