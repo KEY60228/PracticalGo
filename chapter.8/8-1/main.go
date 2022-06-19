9e42c92bdf27e5a51900b2d27b290ee3fac7ed5f
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-06-19 09:49:03 +0900

    8-1. JSON - 明示的なエンコードの防止

--- chapter.8/8-1/main.go
+++ chapter.8/8-1/main.go
@@ -6,14 +6,16 @@ import (
 )
 
 type user struct {
-	UserID   int64
-	UserName string
+	UserID   int64  `json:"user_id"`
+	UserName string `json:"user_name"`
+	X        func() `json:"-"`
 }
 
 func main() {
 	u := user{
 		UserID:   001,
 		UserName: "gopher",
+		X:        func() { fmt.Println("Hello") },
 	}
 	b, _ := json.Marshal(u)
 	fmt.Println(string(b))
