402a7b9b526caf5db932a9409d356340c2f464e6
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-06-22 21:33:00 +0900

    8-4. 固定長 - ianlopshire/go-fixedwidth

--- chapter.8/8-4/main.go
+++ chapter.8/8-4/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strings"
+
+	"github.com/ianlopshire/go-fixedwidth"
 )
 
 type Book struct {
-	ISBN        string
-	PublishDate string
-	Price       string
-	PDF         string
-	EPUB        string
-	EbookPrice  string
+	ISBN        string `fixed:"1,17"`
+	PublishDate string `fixed:"18,25"`
+	Price       int    `fixed:"26,29"`
+	PDF         string `fixed:"30,34,left"`
+	EPUB        string `fixed:"35,39,left"`
+	EbookPrice  int    `fixed:"40,44"`
 }
 
 func main() {
@@ -20,17 +23,11 @@ func main() {
 978-4-87311-878-9201903120000true true 0000`
 
 	for _, line := range strings.Split(s, "\n") {
-		r := []rune(line)
-
-		res := Book{
-			ISBN:        string(r[0:17]),
-			PublishDate: string(r[17:25]),
-			Price:       string(r[25:29]),
-			PDF:         string(r[29:34]),
-			EPUB:        string(r[34:39]),
-			EbookPrice:  string(r[39:43]),
+		var b Book
+		if err := fixedwidth.Unmarshal([]byte(line), &b); err != nil {
+			log.Fatal(err)
 		}
 
-		fmt.Printf("%+v\n", res)
+		fmt.Printf("%+v\n", b)
 	}
 }
