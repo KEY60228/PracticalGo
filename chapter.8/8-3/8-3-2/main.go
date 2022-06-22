0b8f2de8c92aa2c4ed24a423bc8883113391a7b9
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-06-22 21:14:03 +0900

    8-3-3. Excel - 複数レコードを読み込む

--- chapter.8/8-3/8-3-2/main.go
+++ chapter.8/8-3/8-3-2/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/xuri/excelize/v2"
 )
@@ -12,41 +14,39 @@ type Country struct {
 	Population int
 }
 
-func (c Country) HeaderColumns() []interface{} {
-	return []interface{}{"国名", "ISOコード", "人口"}
-}
-
-func (c Country) Columns() []interface{} {
-	return []interface{}{c.Name, c.ISOCode, c.Population}
-}
-
 func main() {
-	lines := []Country{
-		{Name: "アメリカ", ISOCode: "US/USA", Population: 310232863},
-		{Name: "日本", ISOCode: "JP/JPN", Population: 127288000},
-		{Name: "中国", ISOCode: "CN/CHN", Population: 1330044000},
-	}
-
-	f := excelize.NewFile()
-	sw, err := f.NewStreamWriter("Sheet1")
+	f, err := excelize.OpenFile("Book2.xlsx")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for i, line := range lines {
-		if i == 0 {
-			cell, _ := excelize.CoordinatesToCellName(1, i+1)
-			sw.SetRow(cell, line.HeaderColumns())
+	rows, err := f.Rows("Sheet1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var countries []Country
+	for i := 0; rows.Next(); i++ {
+		row, err := rows.Columns()
+		if err != nil {
+			log.Fatal(err)
 		}
-		cell, _ := excelize.CoordinatesToCellName(1, i+2)
-		sw.SetRow(cell, line.Columns())
+
+		if i == 0 {
+			continue
+		}
+
+		population, err := strconv.Atoi(row[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		countries = append(countries, Country{
+			Name:       row[0],
+			ISOCode:    row[1],
+			Population: population,
+		})
 	}
 
-	if err := sw.Flush(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := f.SaveAs("Book2.xlsx"); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(countries)
 }
