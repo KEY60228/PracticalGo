ef235778033fe1625d6934325ec68d94f48d7960
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-06-22 21:27:00 +0900

    8-4. 固定長 - 標準ライブラリ

---
+++ chapter.8/8-4/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Book struct {
+	ISBN        string
+	PublishDate string
+	Price       string
+	PDF         string
+	EPUB        string
+	EbookPrice  string
+}
+
+func main() {
+	s := `978-4-87311-865-9201909174620true true 3696
+978-4-87311-924-3202010102750falsefalse0000
+978-4-87311-878-9201903120000true true 0000`
+
+	for _, line := range strings.Split(s, "\n") {
+		r := []rune(line)
+
+		res := Book{
+			ISBN:        string(r[0:17]),
+			PublishDate: string(r[17:25]),
+			Price:       string(r[25:29]),
+			PDF:         string(r[29:34]),
+			EPUB:        string(r[34:39]),
+			EbookPrice:  string(r[39:43]),
+		}
+
+		fmt.Printf("%+v\n", res)
+	}
+}
