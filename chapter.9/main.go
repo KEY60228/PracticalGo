6ad13572a2aa6099a685929b43b3e729b147575b
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-06-23 20:37:43 +0900

    9-1. データベース - 単一行の取得

--- chapter.9/main.go
+++ chapter.9/main.go
@@ -36,34 +36,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	rows, err := db.QueryContext(context.TODO(), `SELECT user_id, user_name, created_at FROM users ORDER BY user_id;`)
+	userID := "0001"
+	var userName string
+	var createdAt time.Time
+	err = db.QueryRowContext(context.TODO(), `SELECT user_name, created_at FROM users WHERE user_id = $1;`, userID).Scan(&userName, &createdAt)
 	if err != nil {
-		log.Fatalf("query all users; %v", err)
-	}
-	defer rows.Close()
-
-	var users []*User
-	for rows.Next() {
-		var userID string
-		var userName string
-		var createdAt time.Time
-		if err := rows.Scan(&userID, &userName, &createdAt); err != nil {
-			log.Fatalf("scan the user: %v", err)
-		}
-		users = append(users, &User{
-			UserID:    userID,
-			UserName:  userName,
-			CreatedAt: createdAt,
-		})
-	}
-	if err := rows.Close(); err != nil {
-		log.Fatalf("rows close: %v", err)
-	}
-	if err := rows.Err(); err != nil {
-		log.Fatalf("scan users: %v", err)
+		log.Fatalf("query row(user_id=%s): %v", userID, err)
 	}
 
-	for _, user := range users {
-		fmt.Println(user)
+	u := User{
+		UserID:    userID,
+		UserName:  userName,
+		CreatedAt: createdAt,
 	}
+
+	fmt.Println(u)
 }
