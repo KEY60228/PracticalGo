6c6b2383e37d9fbb711ae803f6508efb7d2fafd1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-06-23 21:43:59 +0900

    9-5. ロギング - ドライバーを使ったロギング

--- chapter.9/go.mod
+++ chapter.9/go.mod
@@ -2,16 +2,21 @@ module PracticalGo/chapter.9
 
 go 1.17
 
-require github.com/jackc/pgx/v4 v4.16.1
+require (
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/jackc/pgx/v4 v4.16.1
+)
 
 require (
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- chapter.9/main.go
+++ chapter.9/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"errors"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
@@ -19,27 +18,66 @@ const (
 	PostgresDB       = "go"
 )
 
+type PgTable struct {
+	SchemaName string
+	TableName  string
+}
+
 type User struct {
 	UserID    string
 	UserName  string
 	CreatedAt time.Time
 }
 
+var _ pgx.Logger = (*logger)(nil)
+
+type logger struct{}
+
+func (l *logger) Log(ctx context.Context, level pgx.LogLevel, msg string, data map[string]interface{}) {
+	if msg == "Query" {
+		log.Printf("SQL:\n%v\nARGS:%v\n", data["sql"], data["args"])
+	}
+}
+
 func main() {
 	dsn := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable", PostgresHost, PostgresPort, PostgresUser, PostgresDB, PostgresPassword)
-	db, err := sql.Open("pgx", dsn)
+	ctx := context.Background()
+
+	config, err := pgx.ParseConfig(dsn)
+	if err != nil {
+		log.Fatalf("parse config: %v\n", err)
+	}
+	config.Logger = &logger{}
+
+	conn, err := pgx.ConnectConfig(ctx, config)
+	if err != nil {
+		log.Fatalf("connect: %v\n", err)
+	}
+
+	sql := `SELECT schemaname, tablename FROM pg_tables WHERE schemaname = $1;`
+	args := `information_schema`
+
+	rows, err := conn.Query(ctx, sql, args)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer rows.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if _, err := db.ExecContext(ctx, "SELECT pg_sleep(100)"); err != nil {
-		if errors.Is(err, context.DeadlineExceeded) {
-			log.Println("canceling query")
-		} else {
-			log.Fatal(err)
+	var pgtables []PgTable
+	for rows.Next() {
+		var s string
+		var t string
+		if err := rows.Scan(&s, &t); err != nil {
+			log.Fatalf("Scan: %v\n", err)
 		}
+		pgtables = append(pgtables, PgTable{SchemaName: s, TableName: t})
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, table := range pgtables {
+		fmt.Println(table)
 	}
 }
