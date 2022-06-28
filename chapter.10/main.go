c6e0de54204bd98459722184dda5f1d2deb1068e
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-06-28 22:54:21 +0900

    10-3. HTTP - ファイルのアップロード

--- chapter.10/main.go
+++ chapter.10/main.go
@@ -1,24 +1,45 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/params", handleParams)
+	http.HandleFunc("/file", handleFile)
 	http.ListenAndServe("127.0.0.1:8888", nil)
 }
 
-func handleParams(w http.ResponseWriter, r *http.Request) {
-	word := r.FormValue("searchword")
-	log.Printf("search word = %s\n", word)
-
-	words, ok := r.Form["searchword"]
-	log.Printf("search words = %v has values %v\n", words, ok)
-
-	log.Print("== all queries ==")
-	for key, values := range r.Form {
-		log.Printf("%s: %v\n", key, values)
+func handleFile(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(32 * 1024 * 1024)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+
+	f, h, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Println(h.Filename)
+
+	o, err := os.Create(h.Filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer o.Close()
+
+	_, err = io.Copy(o, f)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	value := r.PostFormValue("data")
+	log.Printf("value = %s\n", value)
 }
