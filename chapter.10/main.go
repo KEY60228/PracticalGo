e3e8ac72e500a5861c2c0422a89eac1642315d69
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-07-03 13:16:52 +0900

    10-5. ミドルウェア - タイムアウト

--- chapter.10/main.go
+++ chapter.10/main.go
@@ -1,92 +1,24 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 )
 
-const (
-	PostgresHost     = "localhost"
-	PostgresPort     = 5432
-	PostgresUser     = "postgres"
-	PostgresPassword = "password"
-	PostgresDB       = "go"
-)
-
-type loggingResponseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func NewLoggingResponseWriter(writer http.ResponseWriter) *loggingResponseWriter {
-	return &loggingResponseWriter{writer, http.StatusOK}
-}
-
-func (lrw *loggingResponseWriter) WriteHeader(code int) {
-	lrw.statusCode = code
-	lrw.ResponseWriter.WriteHeader(code)
-}
-
-func Recovery(next http.Handler) http.Handler {
+func MiddlewareLogging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-				log.Println(err)
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-		}()
+		log.Printf("start %s\n", r.URL)
 		next.ServeHTTP(w, r)
+		log.Printf("finish %s\n", r.URL)
 	})
 }
 
-func NewMiddlewareTx(db *sql.DB) func(http.Handler) http.Handler {
-	return func(wrappedHandler http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			tx, _ := db.Begin()
-			lrw := NewLoggingResponseWriter(w)
-			r = r.WithContext(context.WithValue(r.Context(), "tx", tx))
-
-			wrappedHandler.ServeHTTP(lrw, r)
-
-			statusCode := lrw.statusCode
-			if 200 <= statusCode && statusCode < 400 {
-				log.Println("transaction committed")
-				tx.Commit()
-			} else {
-				log.Println("transaction rolling back due to status code:", statusCode)
-				tx.Rollback()
-			}
-		})
-	}
-}
-
-func extractTx(r *http.Request) *sql.Tx {
-	tx, ok := r.Context().Value("tx").(*sql.Tx)
-	if !ok {
-		panic("transaction middleware is not supported")
-	}
-	return tx
-}
-
 func main() {
-	dsn := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable", PostgresHost, PostgresPort, PostgresUser, PostgresDB, PostgresPassword)
-	db, err := sql.Open("pgx", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	tx := NewMiddlewareTx(db)
-
-	http.Handle("/comments", tx(Recovery(http.HandlerFunc(Comments))))
+	http.Handle("/healthz", http.TimeoutHandler(MiddlewareLogging(http.HandlerFunc(Healthz)), 5, "request timeout"))
 	http.ListenAndServe("localhost:8888", nil)
 }
 
-func Comments(w http.ResponseWriter, r *http.Request) {
-	tx := extractTx(r)
-	// DB処理
-	fmt.Print(tx)
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
