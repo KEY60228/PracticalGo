372716fcba02340debcf7329a0a9095c13bc544d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-07-06 22:52:10 +0900

    11-2. RoundTripper - リトライ

--- chapter.11/main.go
+++ chapter.11/main.go
@@ -2,27 +2,66 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"time"
 )
 
-type basicAuthRoundTripper struct {
+type retryableRoundTripper struct {
 	transport http.RoundTripper
-	username  string
-	password  string
+	attempts  int
+	waitTime  time.Duration
 }
 
-func (rt *basicAuthRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	req.SetBasicAuth(rt.username, rt.password)
-	return rt.transport.RoundTrip(req)
+func (rt *retryableRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	var res *http.Response
+	var err error
+
+	for count := 0; count < rt.attempts; count++ {
+		res, err = rt.transport.RoundTrip(req)
+
+		if !rt.shouldRetry(res, err) {
+			return res, err
+		}
+
+		select {
+		case <-req.Context().Done():
+			return nil, req.Context().Err()
+		case <-time.After(rt.waitTime):
+			// リトライのため待機
+		}
+	}
+
+	return res, err
+}
+
+func (rt *retryableRoundTripper) shouldRetry(res *http.Response, err error) bool {
+	if err != nil {
+		var netErr net.Error
+		if errors.As(err, &netErr) && netErr.Temporary() {
+			return true
+		}
+	}
+
+	if res != nil {
+		if res.StatusCode == 429 || (500 <= res.StatusCode && res.StatusCode <= 504) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func main() {
 	client := &http.Client{
-		Transport: &basicAuthRoundTripper{
+		Transport: &retryableRoundTripper{
 			transport: http.DefaultTransport,
+			attempts:  5,
+			waitTime:  1 * time.Minute,
 		},
 	}
 
