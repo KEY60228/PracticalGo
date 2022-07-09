0a772bafd1930903644b11cc1081a87c6d436808
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-07-09 13:00:02 +0900

    11-3. リトライ - hashicrop/go-retryablehttp

--- chapter.11/main.go
+++ chapter.11/main.go
@@ -2,75 +2,24 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
-	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
 )
 
-type retryableRoundTripper struct {
-	transport http.RoundTripper
-	attempts  int
-	waitTime  time.Duration
-}
-
-func (rt *retryableRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	var res *http.Response
-	var err error
-
-	for count := 0; count < rt.attempts; count++ {
-		res, err = rt.transport.RoundTrip(req)
-
-		if !rt.shouldRetry(res, err) {
-			return res, err
-		}
-
-		select {
-		case <-req.Context().Done():
-			return nil, req.Context().Err()
-		case <-time.After(rt.waitTime):
-			// リトライのため待機
-		}
-	}
-
-	return res, err
-}
-
-func (rt *retryableRoundTripper) shouldRetry(res *http.Response, err error) bool {
-	if err != nil {
-		var netErr net.Error
-		if errors.As(err, &netErr) && netErr.Temporary() {
-			return true
-		}
-	}
-
-	if res != nil {
-		if res.StatusCode == 429 || (500 <= res.StatusCode && res.StatusCode <= 504) {
-			return true
-		}
-	}
-
-	return false
-}
-
 func main() {
-	client := &http.Client{
-		Transport: &retryableRoundTripper{
-			transport: http.DefaultTransport,
-			attempts:  5,
-			waitTime:  1 * time.Minute,
-		},
-	}
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 2
 
-	req, err := http.NewRequestWithContext(context.Background(), "GET", "http://example.com", nil)
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "http://example.com", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	res, err := client.Do(req)
+	res, err := retryClient.StandardClient().Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
