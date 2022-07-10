1c03d9ed096a52f3897238620a53997072b6c51c
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-07-10 09:18:16 +0900

    13-3. テスト - 事前事後の処理

--- chapter.13/main_test.go
+++ chapter.13/main_test.go
@@ -1,6 +1,23 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	setup()
+	defer teardown()
+	m.Run()
+}
+
+func setup() {
+	fmt.Println("tests start")
+}
+
+func teardown() {
+	fmt.Println("tests end")
+}
 
 func TestCalc(t *testing.T) {
 	type args struct {
@@ -32,8 +49,18 @@ func TestCalc(t *testing.T) {
 		},
 	}
 
+	fmt.Println("TestCalc start")
+	defer func() {
+		fmt.Println("TestCalc done")
+	}()
+
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
+			fmt.Printf("test %v start\n", name)
+			defer func() {
+				fmt.Printf("test %v end\n", name)
+			}()
+
 			got, err := Calc(tt.args.a, tt.args.b, tt.args.operator)
 			if (err != nil) != tt.hasErr {
 				t.Errorf("Calc() error = %v, hasErr %v", err, tt.hasErr)
