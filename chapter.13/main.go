8c522e714723b971e01a16d6ed23f2d74697ff9b
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-07-10 09:13:10 +0900

    13-2. テスト - Table Driven Test

--- chapter.13/main.go
+++ chapter.13/main.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
-func Add(a int, b int) int {
-	return a + b
+func Calc(a int, b int, operator string) (int, error) {
+	switch operator {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		if b == 0 {
+			return 0, fmt.Errorf("0除算は未定義です")
+		}
+		return a / b, nil
+	}
+	return 0, fmt.Errorf("予期しない演算子 %v が指定されました", operator)
 }
 
 func main() {
-	fmt.Println(Add(1, 2))
 }
--- chapter.13/main_test.go
+++ chapter.13/main_test.go
@@ -2,9 +2,46 @@ package main
 
 import "testing"
 
-func TestAdd(t *testing.T) {
-	got := Add(1, 2)
-	if got != 3 {
-		t.Errorf("expect 3, but %d", got)
+func TestCalc(t *testing.T) {
+	type args struct {
+		a        int
+		b        int
+		operator string
+	}
+	tests := map[string]struct {
+		args   args
+		want   int
+		hasErr bool
+	}{
+		"足し算": {
+			args: args{
+				a:        10,
+				b:        2,
+				operator: "+",
+			},
+			want:   12,
+			hasErr: false,
+		},
+		"不正な演算子": {
+			args: args{
+				a:        10,
+				b:        2,
+				operator: "?",
+			},
+			hasErr: true,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := Calc(tt.args.a, tt.args.b, tt.args.operator)
+			if (err != nil) != tt.hasErr {
+				t.Errorf("Calc() error = %v, hasErr %v", err, tt.hasErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Calc() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
