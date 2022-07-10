b17ab3bd1126b445945414dfeb7e2a1d8f627c5e
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-07-10 11:20:37 +0900

    13-9. テスト - 時刻

--- chapter.13/main.go
+++ chapter.13/main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"context"
-	"log"
-	"net/http"
+	"fmt"
+	"time"
 )
 
+type contextTimeKey string
+
+const timeKey contextTimeKey = "timeKey"
+
+func CurrentTime(ctx context.Context) time.Time {
+	v := ctx.Value(timeKey)
+	if t, ok := v.(time.Time); ok {
+		return t
+	}
+	return time.Now()
+}
+
+func SetFixTime(ctx context.Context, t time.Time) context.Context {
+	return context.WithValue(ctx, timeKey, t)
+}
+
+func NextMonth(ctx context.Context) time.Month {
+	now := CurrentTime(ctx)
+	return now.AddDate(0, 1, 0).Month()
+}
+
 func main() {
-	client := http.Client{}
-
-	req, err := http.NewRequestWithContext(context.Background(), "GET", "http://example.com/movies/1985", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
+	fmt.Println(NextMonth(context.Background()))
 }
--- chapter.13/main_test.go
+++ chapter.13/main_test.go
@@ -1,46 +1,14 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type testTransport struct {
-	req **http.Request
-	res *http.Response
-	err error
-}
-
-func (t *testTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	*(t.req) = req
-	return t.res, t.err
-}
-
-var _ http.RoundTripper = &testTransport{}
-
-func newTransport(req **http.Request, res *http.Response, err error) http.RoundTripper {
-	return &testTransport{
-		req: req,
-		res: res,
-		err: err,
-	}
-}
-
-func TestHTTPRequest(t *testing.T) {
-	var req *http.Request
-	res := httptest.NewRecorder()
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusOK)
-	res.WriteString(`{"ranking": ["Back to the Future", "Rambo"]}`)
-
-	c := http.Client{
-		Transport: newTransport(&req, res.Result(), nil),
-	}
-
-	r, err := c.Get("http://example.com/movies/1985")
-	assert.Nil(t, err)
-	assert.Equal(t, 200, r.StatusCode)
+func TestNextMonth(t *testing.T) {
+	ctx := SetFixTime(context.Background(), time.Date(1980, time.December, 1, 0, 0, 0, 0, time.Local))
+	assert.Equal(t, time.January, NextMonth(ctx))
 }
