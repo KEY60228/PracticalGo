b82ec4946a294b2e357ba1fca792a9717bab3f08
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-07-18 22:48:51 +0900

    16-2. 排他制御 - チャネルを使った排他制御

--- chapter.16/main.go
+++ chapter.16/main.go
@@ -1,17 +1,48 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+type Account struct {
+	balance  int
+	transfer chan int
+}
+
+func NewAccount() *Account {
+	transfer := make(chan int)
+	r := &Account{0, transfer}
+	go func() {
+		for {
+			amount := <-transfer
+			r.balance += amount
+		}
+	}()
+	return r
+}
+
+func (a Account) GetBalance() int {
+	return a.balance
+}
+
+func (a Account) Transfer(amount int) {
+	a.transfer <- amount
+}
 
 func main() {
-	fmt.Println("ゴルーチンを実行します")
-	go func() {
-		fmt.Println("ゴルーチンが実行しています")
-	}()
+	a := NewAccount()
 
-	fmt.Println("ゴルーチンの終了を待ちます")
-	time.Sleep(time.Second)
-	fmt.Println("ゴルーチンが終了しました")
+	for {
+		var s string
+		fmt.Print("enter command: ")
+		fmt.Scan(&s)
+
+		switch s {
+		case "get":
+			fmt.Println(a.GetBalance())
+		case "add":
+			var i int
+			fmt.Print("enter amount: ")
+			fmt.Scan(&i)
+			a.Transfer(i)
+		}
+	}
 }
