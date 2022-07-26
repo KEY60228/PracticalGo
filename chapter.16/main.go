983d655de10236f985bce9db26fe579e4cb711ae
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-07-26 21:28:45 +0900

    16-5. コンテキスト - ゴルーチン間のイベント伝達

--- chapter.16/main.go
+++ chapter.16/main.go
@@ -2,25 +2,52 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
+	"os/exec"
 	"time"
 )
 
 func main() {
-	recv := make(chan string)
-
-	ctx1, cancel1 := context.WithCancel(context.Background())
-	defer cancel1()
-
-	ctx2, cancel2 := context.WithTimeout(context.Background(), time.Second)
-	defer cancel2()
-
-	select {
-	case <-recv:
-		log.Println("情報があれば受け取りたいが、いつまでもブロックしたくないチャネル")
-	case <-ctx1.Done():
-		log.Println("cancel1()が呼ばれて終了")
-	case <-ctx2.Done():
-		log.Println("タイムアウトで終了")
+	ctx := context.Background()
+	err := runJobs(ctx)
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Println("done")
+}
+
+func runJobs(ctx context.Context) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	ec := make(chan error)
+	done := make(chan struct{})
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			err := exec.CommandContext(ctx, "sleep", "30").Run()
+			if err != nil {
+				ec <- err
+			} else {
+				done <- struct{}{}
+			}
+		}()
+	}
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		ec <- errors.New("accidental error")
+	}()
+
+	for i := 0; i < 10; i++ {
+		select {
+		case err := <-ec:
+			return err
+		case <-done:
+		}
+	}
+
+	return nil
 }
